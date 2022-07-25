a97d69b50968cba4ad26b5793e0380fc0a294242
Nanda <developer.nanda@outlook.com>
2022-07-24 20:11:02 -0500

    add questions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const mongoose = require("mongoose");
 
 require("./src/indexModels");
